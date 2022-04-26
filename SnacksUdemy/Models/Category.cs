30fae2e38263b4d22988e70048a99bd786fe5b5d
Gabriel Brassaroto <Gabriel Brassaroto@DESKTOP-BUP64P5>
2022-04-25 22:08:14 -0300

    Adicionar arquivos de projeto.

---
+++ SnacksUdemy/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace SnacksUdemy.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+        public string  Name { get; set; }
+
+        public string Descripton { get; set; }
+
+        public List<Snack> Snacks { get; set; }
+    }
+}
---
+++ SnacksUdemy/Models/Snack.cs
@@ -0,0 +1,29 @@
+﻿namespace SnacksUdemy.Models
+{
+    public class Snack
+    {
+        public int SnackId { get; set; }
+
+        public string Name { get; set; }
+
+        public string ShortDescripton { get; set; }
+
+        public string DetailedDescripton { get; set;}
+
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+
+        public string ImageThumbnailUrl { get; set; }
+
+        public string IsFavoriteSnack { get; set; }
+
+        public string Stock { get; set; }
+
+
+        //foreign key navegation
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+
+    }
+}
