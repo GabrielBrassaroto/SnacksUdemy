a31e170a8c8da8ae6ed76dca65dff36d39402dc4
Gabriel Brassaroto <Gabriel Brassaroto@DESKTOP-BUP64P5>
2022-04-26 07:50:52 -0300

    Additional Entity Core and Context

---
+++ SnacksUdemy/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SnacksUdemy.Models;
+
+namespace SnacksUdemy
+{
+    public class AppDbContext : DbContext
+    {
+        //contructor EF
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        {
+        }
+
+        //Classes mapeed
+        public DbSet<Category> Categorìes { get; set; }
+        public DbSet<Category> Snacks { get; set; }
+    }
+}
