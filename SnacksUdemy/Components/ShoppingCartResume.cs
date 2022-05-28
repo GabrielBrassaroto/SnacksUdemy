388be1b19414a8c9f6e1deb266479be27f33782e
Gabriel Brassaroto <Gabriel Brassaroto@DESKTOP-BUP64P5>
2022-05-28 18:15:54 -0300

    ViewComponent ShoppingCartItens

---
+++ SnacksUdemy/Components/ShoppingCartResume.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SnacksUdemy.Models;
+using SnacksUdemy.ViewModels;
+
+namespace SnacksUdemy.Components
+{
+    public class ShoppingCartResume : ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartResume(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            // var itens = _shoppingCart.GetShoppingCartItens();
+
+            var itens = new List<ShoppingCartItem>()
+            {
+                new ShoppingCartItem(),
+                new ShoppingCartItem()
+            };
+
+            _shoppingCart.ShoppingCartItems = itens;
+
+            var shoppingCartVM = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetCartBuyTotal()
+            };
+
+            return View(shoppingCartVM);
+        }
+    }
+}
