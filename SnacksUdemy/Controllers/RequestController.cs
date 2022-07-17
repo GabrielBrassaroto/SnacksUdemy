64dfbb2cc8c2c3ac24677057862038fbcdbd137d
Gabriel Brassaroto <Gabriel Brassaroto@DESKTOP-BUP64P5>
2022-07-17 11:21:42 -0300

    Create controller request, and validation

---
+++ SnacksUdemy/Controllers/RequestController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SnacksUdemy.Models;
+using SnacksUdemy.Repositories.Interfaces;
+
+namespace SnacksUdemy.Controllers
+{
+    public class RequestController : Controller
+    {
+        private readonly IRequestRepository _requestRepository;
+
+        private readonly ShoppingCart _shoppingCart;
+        public RequestController(IRequestRepository requestRepository, ShoppingCart shoppingCart)
+        {
+            _requestRepository = requestRepository;
+            _shoppingCart = shoppingCart;
+        }
+
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Request request)
+        {
+            int totalItensRequest = 0;
+            decimal priceTotalRequest = 0.0m;
+
+            //get items from shopping cart
+            List<ShoppingCartItem> items = _shoppingCart.GetShoppingCartItens();
+            _shoppingCart.ShoppingCartItems = items;
+
+            //check if you have items in your cart
+            if (_shoppingCart.ShoppingCartItems.Count == 0)
+            {
+                //modelstate add message in validation model
+                ModelState.AddModelError("", "Your cart is empty add an item to cart...");
+            }
+
+            //calculate cart value
+            foreach (var item in items)
+            {
+                totalItensRequest += item.Quantity;
+                priceTotalRequest += (item.Snack.Price * item.Quantity);
+            }
+
+
+            request.OrderTotalItens = totalItensRequest;
+            request.OrderTotal = priceTotalRequest;
+
+            //validate order data
+            if (ModelState.IsValid)
+            {
+                //create  request and details
+                _requestRepository.CreateRequest(request);
+
+                //define messages client
+                ViewBag.CheckoutCompleteMessage = "Thank you for your request";
+                ViewBag.TotalRequest = _shoppingCart.GetCartBuyTotal();
+
+                ///clear cart shopping cart
+                _shoppingCart.CleanCart();
+
+                return View("~/Views/Request/CheckoutComplete.chtml", request);
+            }
+            return View(request);
+        }
+    }
+}
